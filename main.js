31e1f83780db02a3718ec8f566a260c93e810e6c
rtft <u1025269@utah.edu>
2020-10-17 19:45:27 -0600

    Runs at least a python script now

--- main.js
+++ main.js
@@ -30,6 +30,13 @@ app.on('ready', function(){
     Menu.setApplicationMenu(mainMenu);
 
 
+    var python = require('child_process').spawn('python',['./test.py']);
+    python.stdout.on('data',function(data){
+        console.log("data: ", data.toString('utf8'));
+        //document.getElementById("test_tag").innerHTML = data.toString('utf8');
+    });
+
+
 });
 
 
--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-from __future__ import print_function
-import time
-from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World! This is powered by Python backend."
+import sys
 
-if __name__ == "__main__":
-   print('oh hello')
-    #time.sleep(5)
-    app.run(host='127.0.0.1', port=5000)
+print("hello")
+
+sys.stdout.flush()
